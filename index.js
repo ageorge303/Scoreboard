cfd74830da188ba4de511d144d47c233be8ebfed
ageorge303 <84749560+ageorge303@users.noreply.github.com>
2024-06-04 11:28:59 -0600

    Changes per the requirements
    Changes per the requirements

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-let homeScoreBtnOne = document.getElementById("home-score-btn-1")
-let homeStoreEl =document.getElementById("home-score")
-let homeScore = 0
+let homeScore = 0;
+let awayScore = 0;
 
-function increaseHomeScoreOne(){
+let homeScoreEl = document.getElementById('homeScore');
+let awayScoreEl = document.getElementById('awayScore');
+console.log(homeScoreEl)
+console.log(awayScoreEl)
+
+function homePlusOne() {
     homeScore += 1
-    homeStoreEl.textContent = homeScore
+    homeScoreEl.textContent = homeScore;
+}
+function homePlusTwo() {
+    homeScore += 2
+    homeScoreEl.textContent = homeScore;
+}
+function homePlusThree() {
+    homeScore += 3
+    homeScoreEl.textContent = homeScore;
+}
+function awayPlusOne() {
+    awayScore += 1
+    awayScoreEl.textContent = awayScore;
+}
+function awayPlusTwo() {
+    awayScore += 2
+    awayScoreEl.textContent = awayScore;
+}
+function awayPlusThree() {
+    awayScore += 3
+    awayScoreEl.textContent = awayScore;
 }
